327260ffaeba1ea9cc47cdbadc6dd5644660632d
Numerix <42088870+Numerix76@users.noreply.github.com>
2020-02-29 17:07:46 +0100

    Update custom.lua

--- lua/timerprison/languages/custom.lua
+++ lua/timerprison/languages/custom.lua
@@ -4,11 +4,14 @@ Timer Prison made by Numerix (https://steamcommunity.com/id/numerix/)
 
 --------------------------------------------------------------------------------------------------]]
 
-TimerPrison.Language.VersionCustom = "1.0.0" --DON'T TOUCH THIS
+TimerPrison.Language.VersionCustom = "1.0.1" --DON'T TOUCH THIS
 
 TimerPrison.Language["custom"] = {
 	["Current task: %s for %s seconds"] = "Current task: %s for %s seconds",
 
 	["A new version of the config file is available. Please download it."] = "A new version of the config file is available. Please download it.",
 	["A new version of the language file is available. Please download it."] = "A new version of the language file is available. Please download it.",
+
+	--1.0.1
+	["Lockdown ! Go to cells !"] = "Lockdown ! Go to cells !",
 }
