9db0018bd8dabd311bdd550be60065d44637ec0b
Numerix <42088870+Numerix76@users.noreply.github.com>
2020-03-06 17:56:50 +0100

    Update sh_config.lua
    Added option to set the HUD on left or right

--- lua/timerprison/config/sh_config.lua
+++ lua/timerprison/config/sh_config.lua
@@ -4,13 +4,16 @@ Timer Prison made by Numerix (https://steamcommunity.com/id/numerix/)
 
 --------------------------------------------------------------------------------------------------]]
 
-TimerPrison.Settings.VersionCustom = "1.0.0" --DON'T TOUCH THIS
+TimerPrison.Settings.VersionCustom = "1.0.1" --DON'T TOUCH THIS
 
 --Change the language
 TimerPrison.Settings.Language = "en"
 
 --Sound play on change task
 TimerPrison.Settings.SoundFile = "ambient/alarms/klaxon1.wav"
+
+--Position of the HUD (left, right)
+TimerPrison.Settings.Pos = "left"
 
 TimerPrison.Settings.Work = {
     {
