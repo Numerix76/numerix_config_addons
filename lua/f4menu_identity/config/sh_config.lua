a11e19f9f9f54e44e4ec476de6bb25fab409aab3
Numerix <42088870+Numerix76@users.noreply.github.com>
2020-03-19 18:49:52 +0100

    Added option to disable Quit button

--- lua/f4menu_identity/config/sh_config.lua
+++ lua/f4menu_identity/config/sh_config.lua
@@ -4,7 +4,7 @@ F4Menu made by Numerix (https://steamcommunity.com/id/numerix/)
 
 --------------------------------------------------------------------------------------------------]]
 
-F4Menu.Settings.VersionCustom = "1.0.0" --DON'T TOUCH THIS
+F4Menu.Settings.VersionCustom = "1.0.1" --DON'T TOUCH THIS
 
 --Change the language
 F4Menu.Settings.Language = "en"
@@ -24,6 +24,9 @@ F4Menu.Settings.ShowSlot = true
 
 --Show Model of job ?
 F4Menu.Settings.ShowModel = true
+
+--Show Quit button ?
+F4Menu.Settings.QuitButton = true
 
 --[[ -----------------------------------------------------------------------------------------------
 
