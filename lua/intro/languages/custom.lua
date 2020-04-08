cc32e4e32a14962b84172e1dc21c5523d187539d
Numerix <42088870+Numerix76@users.noreply.github.com>
2020-04-08 11:08:54 +0200

    Translation added

--- lua/intro/languages/custom.lua
+++ lua/intro/languages/custom.lua
@@ -4,7 +4,7 @@ Cinematic Intro made by Numerix (https://steamcommunity.com/id/numerix/)
 
 --------------------------------------------------------------------------------------------------]]
 
-Intro.Language.VersionCustom = "1.0.0" --DON'T TOUCH THIS
+Intro.Language.VersionCustom = "1.0.1" --DON'T TOUCH THIS
 
 Intro.Language["custom"] = {
 	["Start the introduction"] = "Start the introduction",
@@ -12,6 +12,9 @@ Intro.Language["custom"] = {
 	["Okay now go where you want the second camera, then re-enter the command."] = "Okay now go where you want the second camera, then re-enter the command.",
 	["Insert this in sh_config_custom.lua"] = "Insert this in sh_config.lua",
 	["Invalid link"] = "Invalid link",
+	
+	--1.0.1
+	["An error occurred when trying to play music. Please contact server owner if the error persist."] = "An error occurred when trying to play music. Please contact server owner if the error persist.",
 
 	["A new version of the config file is available. Please download it."] = "A new version of the config file is available. Please download it.",
 	["A new version of the language file is available. Please download it."] = "A new version of the language file is available. Please download it.",
