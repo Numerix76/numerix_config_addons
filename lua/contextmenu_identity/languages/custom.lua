1968989aadd63eabeddce0586cda4729c38e9e6a
Numerix <42088870+Numerix76@users.noreply.github.com>
2020-04-18 22:02:30 +0200

    Update custom.lua

--- lua/contextmenu_identity/languages/custom.lua
+++ lua/contextmenu_identity/languages/custom.lua
@@ -4,11 +4,12 @@ ContextMenu Identity made by Numerix (https://steamcommunity.com/id/numerix/)
 
 --------------------------------------------------------------------------------------------------]]
 
-ContextMenuIdentity.Language.VersionCustom = "1.0.0" --DON'T TOUCH THIS
+ContextMenuIdentity.Language.VersionCustom = "1.0.1" --DON'T TOUCH THIS
 
 ContextMenuIdentity.Language["custom"] = {
 	["Accept"] = "Accept",
 
 	["A new version of the config file is available. Please download it."] = "A new version of the config file is available. Please download it.",
 	["A new version of the language file is available. Please download it."] = "A new version of the language file is available. Please download it.",
+	["A new version of the addon is available. Please download it."] = "A new version of the addon is available. Please download it.",
 }
