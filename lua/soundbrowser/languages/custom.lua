ffc89c510d1a10a93ab3e7d11599f4b489490f87
Numerix <42088870+Numerix76@users.noreply.github.com>
2020-04-18 23:14:52 +0200

    Update custom.lua

--- lua/soundbrowser/languages/custom.lua
+++ lua/soundbrowser/languages/custom.lua
@@ -4,7 +4,7 @@ SoundBrowser made by Numerix (https://steamcommunity.com/id/numerix/)
 
 --------------------------------------------------------------------------------------------------]]
 
-SoundBrowser.Language.VersionCustom = "1.0.0" --DON'T TOUCH THIS
+SoundBrowser.Language.VersionCustom = "1.0.1" --DON'T TOUCH THIS
 
 SoundBrowser.Language["custom"] = {
 	["StopSound"] = "StopSound",
@@ -12,4 +12,5 @@ SoundBrowser.Language["custom"] = {
 
 	["A new version of the config file is available. Please download it."] = "A new version of the config file is available. Please download it.",
 	["A new version of the language file is available. Please download it."] = "A new version of the language file is available. Please download it.",
+	["A new version of the addon is available. Please download it."] = "A new version of the addon is available. Please download it.",
 }
