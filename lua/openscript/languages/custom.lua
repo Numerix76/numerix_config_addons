a6cdbe5f8c0473b1126d491412f08957bfc32347
Numerix <42088870+Numerix76@users.noreply.github.com>
2020-04-18 22:52:58 +0200

    Update custom.lua

--- lua/openscript/languages/custom.lua
+++ lua/openscript/languages/custom.lua
@@ -4,7 +4,7 @@ OpenScript made by Numerix (https://steamcommunity.com/id/numerix/)
 
 --------------------------------------------------------------------------------------------------]]
 
-OpenScript.Settings.VersionCustom = "1.0.0" --DON'T TOUCH THIS
+OpenScript.Settings.VersionCustom = "1.0.1" --DON'T TOUCH THIS
 
 OpenScript.Language["custom"] = {
 	["The server open in"] = "The server open in",
@@ -12,4 +12,5 @@ OpenScript.Language["custom"] = {
 
 	["A new version of the config file is available. Please download it."] = "A new version of the config file is available. Please download it.",
 	["A new version of the language file is available. Please download it."] = "A new version of the language file is available. Please download it.",
+	["A new version of the addon is available. Please download it."] = "A new version of the addon is available. Please download it.",
 }
