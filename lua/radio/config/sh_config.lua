9424b8863c5ac31655cf2165cf3153ee84e770f7
Numerix <42088870+Numerix76@users.noreply.github.com>
2020-05-03 16:03:11 +0200

    Update sh_config.lua

--- lua/radio/config/sh_config.lua
+++ lua/radio/config/sh_config.lua
@@ -4,10 +4,21 @@ Radio made by Numerix (https://steamcommunity.com/id/numerix/)
 
 --------------------------------------------------------------------------------------------------]]
 
-Radio.Settings.VersionCustom = "1.0.0" --DON'T TOUCH THIS
+Radio.Settings.VersionCustom = "1.0.1" --DON'T TOUCH THIS
 
 --Change the language
 Radio.Settings.Language = "en"
 
 --Change the distance max of the radio
 Radio.Settings.DistanceSound = 700000
+
+--The player is not able to put his music ?
+Radio.Settings.ActivePreset = false
+
+--Add music preset (only available when Radio.Settings.ActivePreset = true)
+Radio.Settings.Preset = {
+    ["DownTown | Jazzhop"] = "https://www.youtube.com/watch?v=GGBm9gTY2NU&list=RDMM&index=27",
+    ["Cowboy Bebop | Lofi Jazzhop Mix"] = "https://www.youtube.com/watch?v=Z-Rl3LhVakc&list=RDMM&index=27",
+    ["NeverMind"] = "https://gmod-radio-numerix.mtxserv.com/exemple/Dennis%20Lloyd%20-%20NEVERMIND.mp3",
+    ["The Box"] = "https://soundcloud.com/roddyricch/the-box",
+}
