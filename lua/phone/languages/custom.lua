1dffbf47b38186f35fe1b882b67ea2b06ad42a82
Numerix <42088870+Numerix76@users.noreply.github.com>
2020-05-04 15:50:03 +0200

    Update custom.lua

--- lua/phone/languages/custom.lua
+++ lua/phone/languages/custom.lua
@@ -4,7 +4,7 @@ Phone made by Numerix (https://steamcommunity.com/id/numerix/)
 
 --------------------------------------------------------------------------------------------------]]
 
-Phone.Language.VersionCustom = "1.0.1" --DON'T TOUCH THIS
+Phone.Language.VersionCustom = "1.0.2" --DON'T TOUCH THIS
 
 Phone.Language["custom"] = {
 	["What do you want to do ?"] = "What do you want to do ?",
@@ -24,6 +24,9 @@ Phone.Language["custom"] = {
 	["Advert"] = "Advert",
 	["You have sent to the government"] = "You have sent to the government",
 	["Message from"] = "Message from",
+	
+	-- 1.0.2
+	["You have already sent a message !"] = "You have already sent a message !",
 
 	["A new version of the config file is available. Please download it."] = "A new version of the config file is available. Please download it.",
 	["A new version of the language file is available. Please download it."] = "A new version of the language file is available. Please download it.",
