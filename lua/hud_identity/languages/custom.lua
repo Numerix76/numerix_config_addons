6b33045b6fb86b5d5606f7b27a58404546daaf6d
Numerix <42088870+Numerix76@users.noreply.github.com>
2020-06-13 17:26:07 +0200

    Update custom.lua

--- lua/hud_identity/languages/custom.lua
+++ lua/hud_identity/languages/custom.lua
@@ -4,9 +4,11 @@ HUD Identity made by Numerix (https://steamcommunity.com/id/numerix/)
 
 --------------------------------------------------------------------------------------------------]]
 
-HUD.Language.VersionCustom = "1.0.0" --DON'T TOUCH THIS
+HUD.Language.VersionCustom = "1.0.1" --DON'T TOUCH THIS
 
 HUD.Language["custom"] = {
+	["You have less than 20% of battery. Plug your computer to avoid crash."] = "You have less than 20% of battery. Plug your computer to avoid crash.",
+
 	["A new version of the config file is available. Please download it."] = "A new version of the config file is available. Please download it.",
 	["A new version of the language file is available. Please download it."] = "A new version of the language file is available. Please download it.",
 	["A new version of the addon is available. Please download it."] = "A new version of the addon is available. Please download it.",
