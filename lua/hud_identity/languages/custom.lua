8a97224cc6ebd862c9db44bd8eba610b5e201df7
Numerix <42088870+Numerix76@users.noreply.github.com>
2020-06-19 19:21:01 +0200

    Update custom.lua

--- lua/hud_identity/languages/custom.lua
+++ lua/hud_identity/languages/custom.lua
@@ -4,10 +4,23 @@ HUD Identity made by Numerix (https://steamcommunity.com/id/numerix/)
 
 --------------------------------------------------------------------------------------------------]]
 
-HUD.Language.VersionCustom = "1.0.1" --DON'T TOUCH THIS
+HUD.Language.VersionCustom = "1.0.2" --DON'T TOUCH THIS
 
-HUD.Language["custom"] = {
+HUD.Language["default"] = {
 	["You have less than 20% of battery. Plug your computer to avoid crash."] = "You have less than 20% of battery. Plug your computer to avoid crash.",
+
+	--1.0.2
+	["%s has make a warrant on %s for %s"] = "%s has make a warrant on %s for '%s'",
+	["Warrant"] = "Warrant",
+	["%s has make wanted %s for %s"] = "%s has make wanted %s for '%s'",
+	["Wanted"] = "Wanted",
+	["%s unwanted %s"] = "%s unwanted %s",
+	["Unowned"] = "Unowned",
+	["%s has falled from too high"] = "%s has falled from too high",
+	["%s has taked something in his face"] = "%s has taked something in his face",
+	["%s killed %s with %s"] = "%s killed %s with %s",
+	["%s died with %s"] = "%s died with %s",
+	["%s commited to suicide"] = "%s commited to suicide",
 
 	["A new version of the config file is available. Please download it."] = "A new version of the config file is available. Please download it.",
 	["A new version of the language file is available. Please download it."] = "A new version of the language file is available. Please download it.",
