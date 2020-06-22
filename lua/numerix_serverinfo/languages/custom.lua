a880802402cbc37eb3f3defd6a978e1d09de16f8
Numerix <42088870+Numerix76@users.noreply.github.com>
2020-06-22 14:01:30 +0200

    Create custom.lua

---
+++ lua/numerix_serverinfo/languages/custom.lua
@@ -0,0 +1,15 @@
+--[[ ServerInfo --------------------------------------------------------------------------------------
+
+ServerInfo made by Numerix (https://steamcommunity.com/id/numerix/)
+
+--------------------------------------------------------------------------------------------------]]
+
+ServerInfo.Language.VersionCustom = "1.0.0" --DON'T TOUCH THIS
+
+ServerInfo.Language["custom"] = {
+	["Informations of"] = "Informations of",
+	
+	["A new version of the config file is available. Please download it."] = "A new version of the config file is available. Please download it.",
+	["A new version of the language file is available. Please download it."] = "A new version of the language file is available. Please download it.",
+	["A new version of the addon is available. Please download it."] = "A new version of the addon is available. Please download it.",
+}
