027cb87cad86be4acfa3dea98558e53d9c6da756
Numerix <42088870+Numerix76@users.noreply.github.com>
2021-02-21 13:59:14 +0100

    2.0.0

--- lua/radio/languages/custom.lua
+++ lua/radio/languages/custom.lua
@@ -4,56 +4,41 @@ Radio made by Numerix (https://steamcommunity.com/id/numerix/)
 
 --------------------------------------------------------------------------------------------------]]
 
-Radio.Language.VersionCustom = "1.0.5"
+Radio.Language.VersionCustom = "2.0.0"
 
 Radio.Language["custom"] = {
 	["The file does not exist."] = "The file does not exist.",
 	["Check the URL of .mp3."] = "Check the URL of .mp3.",
 	["An error occurred while retrieving the data. Contact an administrator if this persists. Error : %s"] = "An error occurred while retrieving the data. Contact an administrator if this persists. Error : %s",
+	["An error occurred while converting. Contact an administrator if this persists. Error : %s"] = "An error occurred while converting. Contact an administrator if this persists. Error : %s",
+	["An error occurred while retrieving the data. Contact an administrator if this persists."] = "An error occurred while retrieving the data. Contact an administrator if this persists.",
+	["An error occured. Check the message in chat"] = "An error occured. Check the message in chat",
 	["Radio management"] = "Radio management",
 	["Enter a Youtube/MP3/SoundCloud URL"] = "Enter a Youtube/MP3/SoundCloud URL",
 	["URL too long (max: 512 characters)."] = "URL too long (max: 512 characters).",
-	["Invalid link"] = "Invalid link",
 	["Volume"] = "Volume",
-	["Start this music"] = "Start this music",
-	["Cancel"] = "Cancel",
 	["Open menu in car"] = "Open menu in car",
 	["Take the radio from the car"] = "Take the radio from the car",
 	["You have retrieve the radio from the car."] = "You have retrieve the radio from the car.",
 	["There is no radio in the car."] = "There is no radio in the car.",
 	["You are not the owner the car."] = "You are not the owner the car.",
-	["Loading"] = "Loading",
-	["Internet Radio"] = "Internet Radio",
 	["No information"] = "No information",
-	["An error occurred while converting. Contact an administrator if this persists. Error : %s"] = "An error occurred while converting. Contact an administrator if this persists. Error : %s",
 	["Impossible to play the music. Contact an administrator if this persists. (Error : %s, Name : %s)"] = "Impossible to play the music. Contact an administrator if this persists. (Error : %s, Name : %s)",
-	["An error occured. Check the message in chat"] = "An error occured. Check the message in chat",
-	["Click on the play button"] = "Click on the play button",
 	["Now Playing :"] = "Now Playing :",
-	["Youtube"] = "Youtube",
-	["SoundCloud"] = "SoundCloud",
 	["Play"] = "Play",
 	["Stop"] = "Stop",
 	["Pause"] = "Pause",
 	["UnPause"] = "UnPause",
+	["Loop"] = "Loop",
 	["Rainbow mode ?"] = "Rainbow mode ?",
-	["Save Visual"] = "Save Visual",
-	["URL SoundCloud invalid"] = "URL SoundCloud invalid",
 	["Please install a radio in the vehicle."] = "Please install a radio in the vehicle.",
 	["Retrieving data"] = "Retrieving data",
 	["Conversion %d%%"] = "Conversion %d%%",
-	["An error occurred while retrieving the data. Contact an administrator if this persists."] = "An error occurred while retrieving the data. Contact an administrator if this persists.",
-
-	--1.0.1
 	["No result"] = "No result",
 	["Search"] = "Search",
-
-	--1.0.2
+	["Searching..."] = "Searching...",
 	["Please wait the end of last loading"] = "Please wait the end of last loading.",
 	["The duration of the sound is too big. Max : %s"] = "The duration of the sound is too big. Max : %s",
-	["There is no radio from the car."] = "There is no radio from the car.",
-
-	--1.0.3
 	["Voice"] = "Voice",
 	["Connected"] = "Connected",
 	["Station"] = "Station",
@@ -63,19 +48,17 @@ Radio.Language["custom"] = {
 	["No"] = "No",
 	["Connect"] = "Connect",
 	["Disconnect"] = "Disconnect",
+	["Title : "] = "Title : ",
+	["Author : "] = "Author : ",
 	["Station Name"] = "Station Name",
 	["Save"] = "Save",
 	["Auditors : %i"] = "Auditors : %i",
 	["Transmit voice ?"] = "Transmit voice ?",
 	["Waiting for a server music"] = "Waiting for a station music",
-	["Make music loop ?"] = "Make music loop ?",
-	["Visual"] = "Visual",
-	["Take"] = "Take",
 	["Private radio ?"] = "Private radio ?",
 	["Allow buddy (FPP) to use radio ?"] = "Allow buddy (FPP) to use radio ?",
-	
-	--1.0.4
 	["Check your console to have a debug trace"] = "Check your console to have a debug trace",
+	["Radio Internet"] = "Radio Internet",
 
 	["A new version of the config file is available. Please download it."] = "A new version of the config file is available. Please download it.",
 	["A new version of the language file is available. Please download it."] = "A new version of the language file is available. Please download it.",
