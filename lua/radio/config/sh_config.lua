e6e0576a49a1851dec3805af3ca0fe5a3b73c495
Numerix <42088870+Numerix76@users.noreply.github.com>
2022-02-10 22:06:37 +0100

    2.0.2

--- lua/radio/config/sh_config.lua
+++ lua/radio/config/sh_config.lua
@@ -4,16 +4,13 @@ Radio made by Numerix (https://steamcommunity.com/id/numerix/)
 
 --------------------------------------------------------------------------------------------------]]
 
-Radio.Settings.VersionCustom = "2.0.1" --DON'T TOUCH THIS
+Radio.Settings.VersionCustom = "2.0.2" --DON'T TOUCH THIS
 
 --If debug true then if there is a problem you will have the line related in the console
 Radio.Settings.Debug = false
 
 --Change the language
 Radio.Settings.Language = "en"
-
---Put you API Key for youtube (Tutorial to get it : https://www.slickremix.com/docs/get-api-key-for-youtube/)
-Radio.Settings.APIKey = ""
 
 --Change the distance max of the radio
 Radio.Settings.DistanceSoundRadio = 400
@@ -55,88 +52,88 @@ Radio.Settings.VehicleSpawnRadio = false
 Radio.Settings.VehicleSpawnRadioRetrieve = true
 
 hook.Add( "DarkRPFinishedLoading", "Radio:DarkRPInitialized", function() --DON'T TOUCH THIS
-    --Put the TEAM of your job radio
-    Radio.Settings.TeamRadio = TEAM_CITIZEN
+	--Put the TEAM of your job radio
+	Radio.Settings.TeamRadio = TEAM_CITIZEN
 
 end)--DON'T TOUCH THIS
 
 --Add music preset
 Radio.Settings.Preset = {
-    ["https://www.youtube.com/watch?v=4cFD-5w5cyA"] = "Panda Eyes - Crystal Cave",
-    ["https://www.youtube.com/watch?v=LKALcthKSwA"] = "Noisestorm - Sentinel",
-    ["https://gmod-radio-numerix.mtxserv.com/exemple/Dennis%20Lloyd%20-%20NEVERMIND.mp3"] = "NeverMind",
-    ["https://soundcloud.com/roddyricch/the-box"] = "The Box",
+	["https://www.youtube.com/watch?v=4cFD-5w5cyA"] = "Panda Eyes - Crystal Cave",
+	["https://www.youtube.com/watch?v=LKALcthKSwA"] = "Noisestorm - Sentinel",
+	["https://gmod-radio-numerix.mtxserv.com/exemple/Dennis%20Lloyd%20-%20NEVERMIND.mp3"] = "NeverMind",
+	["https://soundcloud.com/roddyricch/the-box"] = "The Box",
 }
 
 Radio.Settings.Navigation =
 {
-    {
-        Enabled = true,
-        Name = "MAIN",
-        Desc = "See the main information of the radio.",
-        DoLoadPanel = "Radio_Main",
-        OnLoadInit = true,
-        Icon = "numerix_radio/home.png",
-        IconName = "your_server_name/home.png",
-        Visible = function(ply, ent) return true end,
-    },
-    {
-        Enabled = true,
-        Name = "SEARCH YOUTUBE",
-        Desc = "Make a search on youtube.",
-        DoLoadPanel = "Radio_Search",
-        type = 1,
-        OnLoadInit = false,
-        Icon = "numerix_radio/youtube.png",
-        IconName = "your_server_name/youtube.png",
-        Visible = function(ply, ent) return (ent.IsServer and !Radio.Settings.ActivePresetOnlyServer) or ( ( ent:IsCarRadio() or ent.ENTRadio or ent.SWEPRadio ) and !Radio.Settings.ActivePresetOnlyRadio ) end,
-    },
-    {
-        Enabled = true,
-        Name = "SEARCH SOUDCLOUD",
-        Desc = "Make a search on soundcloud.",
-        DoLoadPanel = "Radio_Search",
-        type = 2,
-        OnLoadInit = false,
-        Icon = "numerix_radio/soundcloud.png",
-        IconName = "your_server_name/soundcloud.png",
-        Visible = function(ply, ent) return (ent.IsServer and !Radio.Settings.ActivePresetOnlyServer) or ( ( ent:IsCarRadio() or ent.ENTRadio or ent.SWEPRadio ) and !Radio.Settings.ActivePresetOnlyRadio )  end,
-    },
-    {
-        Enabled = true,
-        Name = "PRESET",
-        Desc = "See preset of the server.",
-        DoLoadPanel = "Radio_Preset",
-        OnLoadInit = false,
-        Icon = "numerix_radio/preset.png",
-        IconName = "your_server_name/preset.png",
-        Visible = function(ply, ent) return true end,
-    }, 
-    {
-        Enabled = true,
-        Name = "SETTINGS",
-        Desc = "Settings of the radio.",
-        DoLoadPanel = "Radio_Settings",
-        OnLoadInit = false,
-        Icon = "numerix_radio/settings.png",
-        IconName = "your_server_name/settings.png",
-        Visible = function(ply, ent) return true end,
-    }, 
-    
-    {
-        Enabled = true,
-        Name = "PICK UP",
-        Desc = "Pick Up the radio.",
-        OnLoadInit = false,
-        Icon = "numerix_radio/pickup.png",
-        IconName = "your_server_name/pickup.png",
-        DoFunc = function(menu, ent)
-            net.Start("Radio:Take")
+	{
+		Enabled = true,
+		Name = "MAIN",
+		Desc = "See the main information of the radio.",
+		DoLoadPanel = "Radio_Main",
+		OnLoadInit = true,
+		Icon = "numerix_radio/home.png",
+		IconName = "your_server_name/home.png",
+		Visible = function(ply, ent) return true end,
+	},
+	{
+		Enabled = true,
+		Name = "SEARCH YOUTUBE",
+		Desc = "Make a search on youtube.",
+		DoLoadPanel = "Radio_Search",
+		type = 1,
+		OnLoadInit = false,
+		Icon = "numerix_radio/youtube.png",
+		IconName = "your_server_name/youtube.png",
+		Visible = function(ply, ent) return (ent.IsServer and !Radio.Settings.ActivePresetOnlyServer) or ( ( ent:IsCarRadio() or ent.ENTRadio or ent.SWEPRadio ) and !Radio.Settings.ActivePresetOnlyRadio ) end,
+	},
+	{
+		Enabled = true,
+		Name = "SEARCH SOUDCLOUD",
+		Desc = "Make a search on soundcloud.",
+		DoLoadPanel = "Radio_Search",
+		type = 2,
+		OnLoadInit = false,
+		Icon = "numerix_radio/soundcloud.png",
+		IconName = "your_server_name/soundcloud.png",
+		Visible = function(ply, ent) return (ent.IsServer and !Radio.Settings.ActivePresetOnlyServer) or ( ( ent:IsCarRadio() or ent.ENTRadio or ent.SWEPRadio ) and !Radio.Settings.ActivePresetOnlyRadio )  end,
+	},
+	{
+		Enabled = true,
+		Name = "PRESET",
+		Desc = "See preset of the server.",
+		DoLoadPanel = "Radio_Preset",
+		OnLoadInit = false,
+		Icon = "numerix_radio/preset.png",
+		IconName = "your_server_name/preset.png",
+		Visible = function(ply, ent) return true end,
+	}, 
+	{
+		Enabled = true,
+		Name = "SETTINGS",
+		Desc = "Settings of the radio.",
+		DoLoadPanel = "Radio_Settings",
+		OnLoadInit = false,
+		Icon = "numerix_radio/settings.png",
+		IconName = "your_server_name/settings.png",
+		Visible = function(ply, ent) return true end,
+	}, 
+	
+	{
+		Enabled = true,
+		Name = "PICK UP",
+		Desc = "Pick Up the radio.",
+		OnLoadInit = false,
+		Icon = "numerix_radio/pickup.png",
+		IconName = "your_server_name/pickup.png",
+		DoFunc = function(menu, ent)
+			net.Start("Radio:Take")
 			net.WriteEntity(ent)
 			net.SendToServer()
 
 			menu:Close()
-        end,
-       	Visible = function(ply, ent) return Radio.Settings.EnableSWEP and ent.ENTRadio and !ply:HasWeapon("numerix_radio_swep") end,
-    },
+		end,
+		Visible = function(ply, ent) return Radio.Settings.EnableSWEP and ent.ENTRadio and !ply:HasWeapon("numerix_radio_swep") end,
+	},
 }
