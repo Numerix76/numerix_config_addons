0f10ea0797347679702a969ffc00d8f391a33060
Numerix <42088870+Numerix76@users.noreply.github.com>
2022-02-11 16:31:21 +0100

    1.0.5

--- lua/intro/languages/custom.lua
+++ lua/intro/languages/custom.lua
@@ -4,7 +4,7 @@ Cinematic Intro made by Numerix (https://steamcommunity.com/id/numerix/)
 
 --------------------------------------------------------------------------------------------------]]
 
-Intro.Language.VersionCustom = "1.0.4" --DON'T TOUCH THIS
+Intro.Language.VersionCustom = "1.0.5" --DON'T TOUCH THIS
 
 Intro.Language["custom"] = {
 	["Start the introduction"] = "Start the introduction",
@@ -12,7 +12,7 @@ Intro.Language["custom"] = {
 	["Okay now go where you want the second camera, then re-enter the command."] = "Okay now go where you want the second camera, then re-enter the command.",
 	["Insert this in sh_config_custom.lua"] = "Insert this in sh_config.lua",
 	["Invalid link"] = "Invalid link",
-	
+
 	--1.0.1
 	["An error occurred when trying to play music. Please contact server owner if the error persist."] = "An error occurred when trying to play music. Please contact server owner if the error persist.",
 
@@ -22,10 +22,24 @@ Intro.Language["custom"] = {
 	["An error occurred while retrieving the data. Contact an administrator if this persists. Error : %s"] = "An error occurred while retrieving the data. Contact an administrator if this persists. Error : %s",
 	["URL too long (max: 512 characters)."] = "URL too long (max: 512 characters).",
 	["URL SoundCloud invalid"] = "URL SoundCloud invalid",
-	
+
 	--1.0.4
 	["PlayVideo config enable. You have to put a YouTube URL."] = "PlayVideo config enable. You have to put a YouTube URL.",
-	
+
+	--1.0.5
+	["Connection to the backend"] = "Connecting to the backend",
+	["Starting download"] = "Starting the download on the backend",
+	["Download progress"] = "Download progress on the backend : %.2f%%",
+	["Finished download"] = "Finished the download on the backend",
+	["Starting conversion"] = "Starting the conversion on the backend",
+	["Conversion progress"] = "Conversion progress on the backend : %.2f%%",
+	["Finished conversion"] = "Finished the conversion on the backend",
+	["Logs"] = "Logs",
+	["The module GWSocket is not present on the server"] = "The module GWSocket is not present on the server. Please follow the instructions on https://steamcommunity.com/sharedfiles/filedetails/?id=1989484474 to install it",
+	["Is ready to be used"] = "Is ready to be used",
+	["Failed to setup the addon please check the server console for more information."] = "Failed to setup the addon please check the server console for more information.",
+	["The addon is not ready actually. Please retry later."] = "The addon is not ready actually. Please retry later.",
+
 	["A new version of the config file is available. Please download it."] = "A new version of the config file is available. Please download it.",
 	["A new version of the language file is available. Please download it."] = "A new version of the language file is available. Please download it.",
 	["A new version of the addon is available. Please download it."] = "A new version of the addon is available. Please download it.",
