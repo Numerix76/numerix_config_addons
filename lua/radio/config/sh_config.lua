281a564d18ea805dd02bd8490e2c730d54076695
Numerix <42088870+Numerix76@users.noreply.github.com>
2025-03-21 14:31:28 +0100

    Update sh_config.lua

--- lua/radio/config/sh_config.lua
+++ lua/radio/config/sh_config.lua
@@ -4,7 +4,7 @@ Radio made by Numerix (https://steamcommunity.com/id/numerix/)
 
 --------------------------------------------------------------------------------------------------]]
 
-Radio.Settings.VersionCustom = "2.0.3" --DON'T TOUCH THIS
+Radio.Settings.VersionCustom = "3.0.0" --DON'T TOUCH THIS
 
 --If debug true then if there is a problem you will have the line related in the console
 Radio.Settings.Debug = false
@@ -14,7 +14,7 @@ Radio.Settings.Language = "en"
 
 --If set to true, the addon will use the HTTP library from the game for running music. You will not being able to see the status of the conversion.
 --If set to false, the addon will need GWSocket (see : https://steamcommunity.com/sharedfiles/filedetails/?id=1989484474). You wiil be able to see the status of the conversion.
-Radio.Settings.DegradeMode = true
+Radio.Settings.DegradeMode = false
 
 --Change the distance max of the radio
 Radio.Settings.DistanceSoundRadio = 400
@@ -31,7 +31,7 @@ Radio.Settings.ActivePresetOnlyServer = false
 --Player are able to change the time of the music ? (This can cause a little freeze on all player during this)
 Radio.Settings.Seek = true
 
---Max duration for a sound. (Don't put it too high or when a player will switch between swep to entities or to vehicle all player can freeze)
+--Max duration for a sound.
 Radio.Settings.MaxDuration = 15*60
 
 --Do you want to enable the radio entity ?
@@ -52,8 +52,11 @@ Radio.Settings.Salary = 10
 --All vehicle spawn with a radio in it ?
 Radio.Settings.VehicleSpawnRadio = false
 
---Can radio in vehicle can be retrieve from it ?
-Radio.Settings.VehicleSpawnRadioRetrieve = true
+--Can radio in vehicle can be retrieve from it when they have spawned with it ?
+Radio.Settings.VehicleSpawnRadioRetrieve = false
+
+-- The music of a vehicle is only listenable if the player is inside the vehicle ?
+Radio.Settings.VehicleSoundOnly = true
 
 --Allow the radio to explode if took too much damage ?
 Radio.Settings.ExplodeDamage = true
@@ -67,7 +70,6 @@ Radio.Settings.ServerHealth = 300
 hook.Add( "DarkRPFinishedLoading", "Radio:DarkRPInitialized", function() --DON'T TOUCH THIS
 	--Put the TEAM of your job radio
 	Radio.Settings.TeamRadio = TEAM_CITIZEN
-
 end)--DON'T TOUCH THIS
 
 --Add music preset
@@ -88,7 +90,7 @@ Radio.Settings.Navigation =
 		OnLoadInit = true,
 		Icon = "numerix_radio/home.png",
 		IconName = "your_server_name/home.png",
-		Visible = function(ply, ent) return true end,
+		Visible = function(ply, radio) return true end,
 	},
 	{
 		Enabled = true,
@@ -99,7 +101,7 @@ Radio.Settings.Navigation =
 		OnLoadInit = false,
 		Icon = "numerix_radio/youtube.png",
 		IconName = "your_server_name/youtube.png",
-		Visible = function(ply, ent) return (ent.IsServer and !Radio.Settings.ActivePresetOnlyServer) or ( ( ent:IsCarRadio() or ent.ENTRadio or ent.SWEPRadio ) and !Radio.Settings.ActivePresetOnlyRadio ) end,
+		Visible = function(ply, radio) return (radio:IsServer() and !Radio.Settings.ActivePresetOnlyServer) or (!radio:IsServer() and !Radio.Settings.ActivePresetOnlyRadio) end,
 	},
 	{
 		Enabled = true,
@@ -110,7 +112,7 @@ Radio.Settings.Navigation =
 		OnLoadInit = false,
 		Icon = "numerix_radio/soundcloud.png",
 		IconName = "your_server_name/soundcloud.png",
-		Visible = function(ply, ent) return (ent.IsServer and !Radio.Settings.ActivePresetOnlyServer) or ( ( ent:IsCarRadio() or ent.ENTRadio or ent.SWEPRadio ) and !Radio.Settings.ActivePresetOnlyRadio )  end,
+		Visible = function(ply, radio) return (radio:IsServer() and !Radio.Settings.ActivePresetOnlyServer) or (!radio:IsServer() and !Radio.Settings.ActivePresetOnlyRadio) end,
 	},
 	{
 		Enabled = true,
@@ -120,7 +122,7 @@ Radio.Settings.Navigation =
 		OnLoadInit = false,
 		Icon = "numerix_radio/preset.png",
 		IconName = "your_server_name/preset.png",
-		Visible = function(ply, ent) return true end,
+		Visible = function(ply, radio) return true end,
 	}, 
 	{
 		Enabled = true,
@@ -130,8 +132,8 @@ Radio.Settings.Navigation =
 		OnLoadInit = false,
 		Icon = "numerix_radio/settings.png",
 		IconName = "your_server_name/settings.png",
-		Visible = function(ply, ent) return true end,
-	}, 
+		Visible = function(ply, radio) return true end,
+	},
 	
 	{
 		Enabled = true,
@@ -140,13 +142,13 @@ Radio.Settings.Navigation =
 		OnLoadInit = false,
 		Icon = "numerix_radio/pickup.png",
 		IconName = "your_server_name/pickup.png",
-		DoFunc = function(menu, ent)
+		DoFunc = function(menu, radio)
 			net.Start("Radio:Take")
-			net.WriteEntity(ent)
+			net.WriteEntity(radio:GetParent())
 			net.SendToServer()
 
 			menu:Close()
 		end,
-		Visible = function(ply, ent) return Radio.Settings.EnableSWEP and ent.ENTRadio and !ply:HasWeapon("numerix_radio_swep") end,
+		Visible = function(ply, radio) return Radio.Settings.EnableSWEP and !radio:IsWeapon() and !radio:IsServer() and !ply:HasWeapon("numerix_radio_swep") end,
 	},
 }
